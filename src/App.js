cffce1144cd67d9847d0071623ef9e9776e39b63
Supriyo Saha <mm.saha1982@gmail.com>
2023-07-02 21:37:52 +0530

    sidebar added

--- src/App.js
+++ src/App.js
@@ -8,9 +8,10 @@ import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Ka
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import { useStateContext } from "./contexts/ContextProvider";
 
 const App = () => {
-  const activeMenu = true;
+  const { activeMenu } = useStateContext();
 
   return (
     <div>
