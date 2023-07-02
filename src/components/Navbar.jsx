8f81fad5c0005cca275e45f97d0222e7532bd66c
Supriyo Saha <mm.saha1982@gmail.com>
2023-07-02 22:50:19 +0530

    icon size fix in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,9 +21,8 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
       <span
         style={{ background: dotColor }}
         className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
-      >
-        {icon}
-      </span>
+      />
+      {icon}
     </button>
   </TooltipComponent>
 );
@@ -55,7 +54,7 @@ const Navbar = () => {
       setActiveMenu(true);
     }
   }, [screenSize]);
-  
+
   return (
     <div className="flex justify-between p-2 md:mx-6 relative">
       <NavButton
